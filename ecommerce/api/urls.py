7bf942f6476ad00720ff263c8815100904f881c8
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2025-01-29 20:37:14 +0300

    added defaultrouter

---
+++ ecommerce/api/urls.py
@@ -0,0 +1,12 @@
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewset,OrderViewset,SignupView,LoginView
+from django.urls import path,include
+router=DefaultRouter()
+router.register(r'products',ProductViewset)
+router.register(r'orders',OrderViewset)
+
+urlpatterns=[
+    path("",(router.urls)),
+    path('signup/',SignupView.as_view()),
+    path('login/',LoginView.as_view())
+]
